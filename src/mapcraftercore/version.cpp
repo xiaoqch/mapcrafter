6738e3462c3cb741b0de940a7c6e36fcc64b7fcf
Michael <michaellavaire@gmail.com>
2022-06-08 09:27:07 -0400

    Update blocks 1.19 and fix some model texturing

--- src/mapcraftercore/version.cpp
+++ src/mapcraftercore/version.cpp
@@ -3,5 +3,5 @@
 namespace mapcrafter {
 	const char* MINECRAFT_VERSION = "1.19.0";
 	const char* MAPCRAFTER_VERSION = "3.1";
-	const char* MAPCRAFTER_GITVERSION = "v.2.3.1-328-gefd4a59";
+	const char* MAPCRAFTER_GITVERSION = "v.2.3.1-340-g4847c27";
 };
